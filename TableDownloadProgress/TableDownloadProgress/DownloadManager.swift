bdcbe50b3afd5b6ce313f72127ab1298895f21c5
Basheer Ahamed <mca.basheer@gmail.com>
2017-08-24 11:48:13 +0800

    Add files via upload
    Project setup.

---
+++ TableDownloadProgress/TableDownloadProgress/DownloadManager.swift
@@ -0,0 +1,92 @@
+//
+//  DownloadManager.swift
+//  TableDownloadProgress
+//
+//  Created by Basheer on 23/8/17.
+//  Copyright © 2017 Basheer. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession {
+    func getSessionDescription () -> Int {
+        return Int(self.sessionDescription!)!
+    }
+}
+
+class DownloadManager : NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
+    
+    static var shared = DownloadManager()
+    var identifier : Int = -1
+    typealias ProgressHandler = (Int, Float) -> ()
+    
+    var onProgress : ProgressHandler? {
+        didSet {
+            if onProgress != nil {
+                let _ = activate()
+            }
+        }
+    }
+    
+    override private init() {
+        super.init()
+    }
+    
+    func activate() -> URLSession {
+        let config = URLSessionConfiguration.background(withIdentifier: "\(Bundle.main.bundleIdentifier!).background.\(NSUUID.init())")
+        
+        let urlSession = URLSession(configuration: config, delegate: self, delegateQueue: OperationQueue())
+        urlSession.sessionDescription = String(identifier)
+        return urlSession
+    }
+    
+    private func calculateProgress(session : URLSession, completionHandler : @escaping (Int, Float) -> ()) {
+        session.getTasksWithCompletionHandler { (tasks, uploads, downloads) in
+            let progress = downloads.map({ (task) -> Float in
+                if task.countOfBytesExpectedToReceive > 0 {
+                    return Float(task.countOfBytesReceived) / Float(task.countOfBytesExpectedToReceive)
+                } else {
+                    return 0.0
+                }
+            })
+
+            completionHandler(session.getSessionDescription(), progress.reduce(0.0, +))
+        }
+    }
+    
+    func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didFinishDownloadingTo location: URL){
+        
+        let fileName = downloadTask.originalRequest?.url?.lastPathComponent
+        let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        let documentDirectoryPath:String = path[0]
+        let fileManager = FileManager()
+        var destinationURLForFile = URL(fileURLWithPath: documentDirectoryPath.appending("/\(location.relativeString)"))
+        do {
+            try fileManager.createDirectory(at: destinationURLForFile, withIntermediateDirectories: true, attributes: nil)
+            destinationURLForFile.appendPathComponent(String(describing: fileName!))
+            try fileManager.moveItem(at: location, to: destinationURLForFile)
+        }catch(let error){
+            print(error)
+        }
+        
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        if totalBytesExpectedToWrite > 0 {
+            if let onProgress = onProgress {
+                calculateProgress(session: session, completionHandler: onProgress)
+            }
+            let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+            debugPrint("Progress \(downloadTask) \(progress)")
+            
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        debugPrint("Task completed: \(task), error: \(String(describing: error))")
+    }
+    
+}
---
+++ TableDownloadProgress/TableDownloadProgress/MasterViewController.swift
@@ -0,0 +1,175 @@
+//
+//  MasterViewController.swift
+//  TableDownloadProgress
+//
+//  Created by Basheer on 23/8/17.
+//  Copyright © 2017 Basheer. All rights reserved.
+//
+
+import UIKit
+
+enum DownloadStatus {
+    case none
+    case inProgress
+    case completed
+    case failed
+}
+struct item {
+    var title : String!
+    let link = "https://www.videvo.net/?page_id=123&desc=OldFashionedFilmLeaderCountdownVidevo.mov&vid=1351"
+    var downloadStatus : DownloadStatus = .none
+
+    init(title: String) {
+        self.title = title
+    }
+}
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [Any]()
+    var items = [item]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = editButtonItem
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        navigationItem.rightBarButtonItem = addButton
+//        if let split = splitViewController {
+//            let controllers = split.viewControllers
+//            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+//        }
+        
+        items.append(item(title: "Video 1"))
+        items.append(item(title: "Video 2"))
+        items.append(item(title: "Video 3"))
+        items.append(item(title: "Video 4"))
+        
+        self.tableView.rowHeight = 100.0
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @objc
+    func insertNewObject(_ sender: Any) {
+        objects.insert(NSDate(), at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+
+    // MARK: - Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row] as! NSDate
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        if item.downloadStatus != .completed {
+            let progressRing = UICircularProgressRingView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+            // Change any of the properties you'd like
+            progressRing.maxValue = 100
+            progressRing.innerRingColor = UIColor.blue
+            cell.tag = indexPath.row
+            cell.accessoryType = UITableViewCellAccessoryType.none
+            cell.accessoryView = progressRing
+        }
+        else {
+            cell.accessoryView = nil
+        }
+        
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        
+        var item = items[indexPath.row]
+        if item.downloadStatus == .inProgress || item.downloadStatus == .completed {
+            print("video already downloaded")
+        }
+        else {
+            let url = URL(string: item.link)!
+            DownloadManager.shared.identifier = indexPath.row
+            let downloadTaskLocal =  DownloadManager.shared.activate().downloadTask(with: url)
+            downloadTaskLocal.resume()
+            
+            DownloadManager.shared.onProgress = { (row, progress) in
+                //print("Downloading for \(row) with progress \(progress)")
+                
+                DispatchQueue.main.async {
+                    let indexpath = IndexPath.init(row: row, section: 0)
+                    let cell = self.tableView.cellForRow(at: indexpath)
+                    print("downloading for cell \(String(describing: cell?.tag))")
+                    if progress <= 1.0 {
+                        
+                        let progressRing = cell?.accessoryView as! UICircularProgressRingView
+                        progressRing.setProgress(value: CGFloat(progress * 100), animationDuration: 0.2)
+                        
+                        if progress == 1.0 {
+                            item.downloadStatus = .completed
+                            cell?.textLabel?.text = "Download Complete"
+                        }
+                        else {
+                            cell?.textLabel?.text = "Download In Progress"
+                        }
+                        
+                    }
+                }
+                
+            }
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+            
+        }
+    }
+
+
+}
+
